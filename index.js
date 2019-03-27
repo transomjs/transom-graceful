ed03d5cf3bba5eb6198131e7ed2b0a591adce0c9
Mark Voorberg <mark@binaryops.ca>
2019-03-27 14:42:17 -0700

    use settings from either the API definition, or the call to initialize

--- index.js
+++ index.js
@@ -1,134 +1,123 @@
 'use strict';
 const debug = require('debug')('transom:graceful');
-const { promisify } = require('es6-promisify');
+const {
+	promisify
+} = require('es6-promisify');
 const stoppable = require('stoppable');
 
 function TransomGraceful() {
-    let isStopping = false;
+	let isStopping = false;
 
-    const SUCCESS_RESPONSE = JSON.stringify({
-        status: 'ok'
-    });
+	const SUCCESS_RESPONSE = JSON.stringify({
+		status: 'ok'
+	});
 
-    const STOPPING_RESPONSE = JSON.stringify({
-        status: 'error',
-        error: 'server is shutting down'
-    });
+	const STOPPING_RESPONSE = JSON.stringify({
+		status: 'error',
+		error: 'server is shutting down'
+	  });
 
-    function noopResolve() {
-        return Promise.resolve();
-    }
+	function noopResolve() {
+		return Promise.resolve();
+	}
 
-    this.initialize = function(server, options) {
-        return new Promise(function(resolve, reject) {
-            debug('initializing transomGraceful');
-            options = options || {};
+	this.initialize = function (server, options) {
+		return new Promise(function (resolve, reject) {
+			debug('initializing transomGraceful');
 
-            const signals = options.signals || ['SIGINT', 'SIGTERM'];
-            const timeout = options.timeout || 1000;
-            const beforeShutdown = options.beforeShutdown || noopResolve;
-            const onSignal = options.onSignal || noopResolve;
-            const afterShutdown = options.afterShutdown || noopResolve;
+			// Combine the API definition section for graceful with the options...
+			const gracefulDefn = server.registry.get('transom-config.definition.graceful', {});
+			options = Object.assign({}, gracefulDefn, options);
 
-            // Insert a middleware to stop responding if shutting down.
-            server.use(function(req, res, next) {
-                if (isStopping) {
-                    res.statusCode = 503;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.end(STOPPING_RESPONSE);
-                    return;
-                }
-                next();
-            });
+			const signals = options.signals || ['SIGINT', 'SIGTERM'];
+			const timeout = options.timeout || 1000;
+			const beforeShutdown = options.beforeShutdown || noopResolve;
+			const onSignal = options.onSignal || noopResolve;
+			const afterShutdown = options.afterShutdown || noopResolve;
+			const serviceUnavailable = options.serviceUnavailableWhileStopping === false ? false : true;
 
-            function gracefulHealthCheck(healthUrl, check) {
-                return function(req, res, next) {
-                    // checks if the system is healthy, like the db connection
-                    // is live or an external system / API is available.
-                    // Health check resolves, if healthy, rejects if not.
-                    if (isStopping) {
-                        debug(
-                            'HealthCheck(%s), server is shutting-down.',
-                            healthUrl
-                        );
-                        res.statusCode = 503;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.end(STOPPING_RESPONSE);
-                        return;
-                    }
-                    if (typeof check === 'function') {
-                        debug('HealthCheck(%s), checking...', healthUrl);
-                        return check(server, req, res, next);
-                    } else {
-                        debug('HealthCheck(%s), server is running.', healthUrl);
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.end(SUCCESS_RESPONSE);
-                        return;
-                    }
-                };
-            }
+			if (serviceUnavailable) {
+				// Insert a middleware to stop responding if shutting down.
+				server.use(function (req, res, next) {
+					if (isStopping) {
+						res.statusCode = 503;
+						res.setHeader('Content-Type', 'application/json');
+						res.end(STOPPING_RESPONSE);
+						return;
+					}
+					next();
+				});
+			}
 
-            // Add healthcheck routes to the server
-            if (
-                typeof options.healthChecks === 'object' &&
-                Object.keys(options.healthChecks).length > 0
-            ) {
-                Object.keys(options.healthChecks).map(key => {
-                    server.get(
-                        key,
-                        gracefulHealthCheck(key, options.healthChecks[key])
-                    );
-                });
-            }
+			function gracefulHealthCheck(healthUrl, check) {
+				return function (req, res, next) {
+					// checks if the system is healthy, like the db connection 
+					// is live or an external system / API is available.
+					// Health check resolves, if healthy, rejects if not.
+					if (isStopping) {
+						debug('HealthCheck(%s), server is shutting-down.', healthUrl);
+						res.statusCode = 503;
+						res.setHeader('Content-Type', 'application/json');
+						res.end(STOPPING_RESPONSE);
+						return;
+					}
+					if (typeof check === 'function') {
+						debug('HealthCheck(%s), checking...', healthUrl);
+						return check(server, req, res, next);
+					} else {
+						debug('HealthCheck(%s), server is running.', healthUrl);
+						res.statusCode = 200;
+						res.setHeader('Content-Type', 'application/json');
+						res.end(SUCCESS_RESPONSE);
+						return;
+					}
+				}
+			}
 
-            // Stop accepting new connections and closes existing, idle connections
-            //  (including keep-alives) without killing requests that are in-flight.
-            stoppable(server.restify, timeout);
-            const serverStop = promisify(server.restify.stop).bind(server);
+			// Add healthcheck routes to the server
+			if (typeof options.healthChecks === 'object' && Object.keys(options.healthChecks).length > 0) {
+				Object.keys(options.healthChecks).map((key) => {
+					server.get(key, gracefulHealthCheck(key, options.healthChecks[key]));
+				});
+			}
 
-            function gracefulCleanup(signal) {
-                return function() {
-                    // Cleanup resources like databases or file descriptors.
-                    if (!isStopping) {
-                        isStopping = true;
-                        debug(
-                            'Server received %s interrupt, starting cleanup.',
-                            signal
-                        );
+			// Stop accepting new connections and closes existing, idle connections
+			//  (including keep-alives) without killing requests that are in-flight.
+			stoppable(server.restify, timeout);
+			const serverStop = promisify(server.restify.stop).bind(server);
 
-                        beforeShutdown(server)
-                            .then(() => serverStop())
-                            .then(() => onSignal(server))
-                            .then(() => afterShutdown(server))
-                            .then(() => {
-                                signals.forEach(sig => {
-                                    debug('Removing %s listener.', sig);
-                                    process.removeListener(
-                                        sig,
-                                        gracefulCleanup
-                                    );
-                                });
-                                debug(
-                                    'Killing server process %d.',
-                                    process.pid
-                                );
-                                process.kill(process.pid, signal);
-                            })
-                            .catch(error => {
-                                debug('Error during shutdown', error);
-                                process.exit(1);
-                            });
-                    }
-                };
-            }
-            signals.map(signal => {
-                process.on(signal, gracefulCleanup(signal));
-            });
+			function gracefulCleanup(signal) {
+				return function () {
+					// Cleanup resources like databases or file descriptors.
+					if (!isStopping) {
+						isStopping = true;
+						debug('Server received %s interrupt, starting cleanup.', signal);
 
-            resolve();
-        });
-    };
+						beforeShutdown(server)
+							.then(() => serverStop())
+							.then(() => onSignal(server))
+							.then(() => afterShutdown(server))
+							.then(() => {
+								signals.forEach((sig) => {
+									debug('Removing %s listener.', sig);
+									process.removeListener(sig, gracefulCleanup);
+								});
+								debug('killing server process %d.', process.pid);
+								process.kill(process.pid, signal);
+							}).catch((error) => {
+								debug('Error during shutdown', error)
+								process.exit(1)
+							});
+					}
+				}
+			}
+			signals.map((signal) => {
+				process.on(signal, gracefulCleanup(signal));
+			});
+
+			resolve();
+		});
+	}
 }
 
 module.exports = new TransomGraceful();
