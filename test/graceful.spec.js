48e0b6bdc8788e1d0007c845e06ff01f6e6dc736
Mark Voorberg <mark@binaryops.ca>
2024-01-16 21:48:52 -0800

    updates to node 14+

--- test/graceful.spec.js
+++ test/graceful.spec.js
@@ -1,24 +1,24 @@
 "use strict";
-const expect = require('chai').expect;
-const sinon = require('sinon');
-const TransomGraceful = require('../index');
 
-describe('TransomGraceful', function () {
+let expect;
+const sinon = require("sinon");
+const TransomGraceful = require("../index");
 
-    beforeEach(function () {
-    });
+describe("TransomGraceful", function () {
+  beforeEach(() => {
+    // Use a dynamic import for the chai ES module!
+    return import("chai").then((chai) => (expect = chai.expect));
+  });
 
-    afterEach(function () {
-    });
-    
-    it('looks like a Transom plugin', function (done) {
-        expect(TransomGraceful).to.be.an.instanceOf(Object);
-        expect(TransomGraceful.initialize).to.be.an.instanceOf(Function);
-        done();
-    });
+  afterEach(function () {});
 
-    it('needs tests', function (done) {
-        done();
-    });
+  it("looks like a Transom plugin", function (done) {
+    expect(TransomGraceful).to.be.an.instanceOf(Object);
+    expect(TransomGraceful.initialize).to.be.an.instanceOf(Function);
+    done();
+  });
 
+  it("needs tests", function (done) {
+    done();
+  });
 });
